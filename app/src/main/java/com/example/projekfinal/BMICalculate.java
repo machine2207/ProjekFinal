cb024d2af2eeb02c1f55b83c56280dc4626c4b0f
machine2207 <gun.machine22072003@gmailcom>
2023-05-27 00:40:40 +0800

    BMI Calculator

---
+++ app/src/main/java/com/example/projekfinal/BMICalculate.java
@@ -0,0 +1,14 @@
+package com.example.projekfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BMICalculate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmicalculate);
+    }
+}
